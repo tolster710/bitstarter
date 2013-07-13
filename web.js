757953a6c8d200c5c514803ccde50745a1fce80a
tolster710 <johnftolly@gmail.com>
2013-07-13 20:16:26 +0000

    Added the parenthesis back in

--- web.js
+++ web.js
@@ -7,8 +7,10 @@ app.get('/', function(request, response) {
 var buf = fs.readFileSync('index.html');
  //response.send('Hello World2!');
 response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
