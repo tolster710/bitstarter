bb4b8c9bc5822245cdf81c8dfc6d86ee4de7f4ef
tolster710 <johnftolly@gmail.com>
2013-07-13 19:58:36 +0000

    Added buffer function to web.js

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  //response.send('Hello World2!');
+response.send(buf.toString(fs.readFileSync("index.html")));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
