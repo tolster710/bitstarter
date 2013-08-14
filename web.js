4a8539f469957ea39f2ecd72bb5902cfeab22a65
tolster710 <johnftolly@gmail.com>
2013-08-14 17:50:49 -0400

    hbd - website created

--- web.js
+++ web.js
@@ -2,12 +2,19 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
 var buf = fs.readFileSync('index.html');
  //response.send('Hello World2!');
 response.send(buf.toString());
 });
+
+app.get('/hbd', function(request, response) {
+	var buf = fs.readFileSync('hbd.html');
+	response.send(buf.toString());
+	});
+	
 
 var port = process.env.PORT || 8080;
 
